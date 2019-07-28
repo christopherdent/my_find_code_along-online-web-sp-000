9f353a684680c4d7c2b6286ca05d2982e4e6f092
Christopher Dent <christopherdent01@gmail.com>
2019-07-28 23:14:02 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,10 +2,12 @@ require 'pry'
 
 def my_find(collection)
 i = 0
-while i < collection.length
+  while i < collection.length
 
-  yield(collection[i])
+    yield(collection[i])
 
-  i += 1
-end 
+    i += 1
+  end 
+
+
 end
